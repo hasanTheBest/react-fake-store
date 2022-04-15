f30949a838be0bb3a01590e7c1efc71d0a5be86a
hasanTheBest <rainbowmahmud@gmail.com>
2022-04-16 02:35:57 +0600

    firebase deploy added

--- src/components/Login/SignUp.js
+++ src/components/Login/SignUp.js
@@ -6,6 +6,7 @@ import {
 } from "react-firebase-hooks/auth";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
+import toast, { Toaster } from "react-hot-toast";
 
 const SignUp = () => {
   const [name, setName] = useState({});
@@ -114,7 +115,7 @@ const SignUp = () => {
     e.preventDefault();
 
     if (name.error || email.error || password.error || confirmPassErr) {
-      alert("Try fixing error. Then submit");
+      toast("Try fixing error. Then submit");
       return;
     }
 
@@ -122,145 +123,141 @@ const SignUp = () => {
     await createUserWithEmailAndPassword(email.data, password.data);
     await updateProfile({ displayName: name.data });
     navigate(from, { replace: true });
-
-    // update user profile display name
-    // await updateProfile({ displayName: name.data });
   };
 
-  //
-  // if (error || errorProfile)
-  //   console.error(error.message || errorProfile.message);
-
-  if (user) {
-    navigate(from, { replace: true });
+  if (error || errorProfile) {
+    toast(error?.message || errorProfile?.message);
   }
 
   return (
-    <div className="p-8 max-w-2xl mx-auto mt-12 rounded shadow-md border border-slate-700 bg-slate-800">
-      <h2 className="text-center mb-8 text-3xl text-slate-500 font-semibold">
-        Sign Up
-      </h2>
+    <>
+      <div className="p-8 max-w-2xl mx-auto mt-12 rounded shadow-md border border-slate-700 bg-slate-800">
+        <h2 className="text-center mb-8 text-3xl text-slate-500 font-semibold">
+          Sign Up
+        </h2>
 
-      <form
-        className="mb-6 text-slate-400 font-semibold text-lg"
-        onSubmit={handleSignUpFormSubmit}
-      >
-        {/* Name */}
-        <div className="mb-4 w-full flex flex-col">
-          <label htmlFor="name" className="mb-2">
-            Name
-          </label>
+        <form
+          className="mb-6 text-slate-400 font-semibold text-lg"
+          onSubmit={handleSignUpFormSubmit}
+        >
+          {/* Name */}
+          <div className="mb-4 w-full flex flex-col">
+            <label htmlFor="name" className="mb-2">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Your Name"
+              className="p-3 border border-slate-600 bg-slate-700 focus:bg-slate-600 focus:text-slate-200 flex-grow rounded shadow"
+              onBlur={(e) => validateInputField(e, "name")}
+            />
+            {/* Error message */}
+            {!!name.error && (
+              <p className="text-red-500 mt-2 text-sm font-normal">
+                {name.error}
+              </p>
+            )}
+          </div>
+
+          {/* Email */}
+          <div className="mb-4 w-full flex flex-col">
+            <label htmlFor="email" className="mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Your Email"
+              className="p-3 border border-slate-600 bg-slate-700 focus:bg-slate-600 focus:text-slate-200 flex-grow rounded shadow"
+              onBlur={(e) => validateInputField(e, "email")}
+            />
+
+            {/* Error message */}
+            {!!email.error && (
+              <p className="text-red-500 mt-2 text-sm font-normal">
+                {email.error}
+              </p>
+            )}
+          </div>
+
+          {/* Password */}
+          <div className="mb-4 w-full flex flex-col">
+            <label htmlFor="password" className="mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Your Password"
+              className="p-3 border border-slate-600 bg-slate-700 focus:bg-slate-600 focus:text-slate-200 flex-grow rounded shadow"
+              onBlur={(e) => validateInputField(e, "password")}
+            />
+            {/* Error message */}
+            {!!password.error && (
+              <p className="text-red-500 mt-2 text-sm font-normal">
+                {password.error}
+              </p>
+            )}
+          </div>
+
+          {/* Confirm Password */}
+          <div className="mb-4 w-full flex flex-col">
+            <label htmlFor="confirmPassword" className="mb-2">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              placeholder="Re-type your password"
+              className="p-3 border border-slate-600 bg-slate-700 focus:bg-slate-600 focus:text-slate-200 flex-grow rounded shadow"
+              onBlur={(e) => validateInputField(e, "confirmPassword")}
+            />
+            {/* Error message */}
+            {!!confirmPassErr && (
+              <p className="text-red-500 mt-2 text-sm font-normal">
+                {confirmPassErr}
+              </p>
+            )}
+          </div>
+
+          {/* submit button */}
           <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Your Name"
-            className="p-3 border border-slate-600 bg-slate-700 focus:bg-slate-600 focus:text-slate-200 flex-grow rounded shadow"
-            onBlur={(e) => validateInputField(e, "name")}
+            type="submit"
+            value="Sign Up"
+            className="w-full p-3 text-center shadow-md cursor-pointer rounded bg-sky-800 hover:bg-sky-700 text-sky-300 hover:text-sky-200 transition-all mt-4"
           />
-          {/* Error message */}
-          {!!name.error && (
-            <p className="text-red-500 mt-2 text-sm font-normal">
-              {name.error}
-            </p>
-          )}
-        </div>
+        </form>
 
-        {/* Email */}
-        <div className="mb-4 w-full flex flex-col">
-          <label htmlFor="email" className="mb-2">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Your Email"
-            className="p-3 border border-slate-600 bg-slate-700 focus:bg-slate-600 focus:text-slate-200 flex-grow rounded shadow"
-            onBlur={(e) => validateInputField(e, "email")}
-          />
+        <p className="my-3 text-slate-400">
+          Already a user?{" "}
+          <Link to="/login" className="text-sky-400">
+            Sing In
+          </Link>
+        </p>
 
-          {/* Error message */}
-          {!!email.error && (
-            <p className="text-red-500 mt-2 text-sm font-normal">
-              {email.error}
-            </p>
-          )}
-        </div>
+        {/* one click log in  */}
+        <div className="px-4 flex flex-col items-center">
+          <span className="relative text-slate-500 mb-2 before:w-full after:w-full before:h-0.5 after:h-0.5 before:bg-slate-700 after:bg-slate-700 before:absolute after:absolute before:-left-full after:-right-full before:top-1/2 after:top-1/2 before:-ml-2 after:-mr-2">
+            Or Sign up with
+          </span>
 
-        {/* Password */}
-        <div className="mb-4 w-full flex flex-col">
-          <label htmlFor="password" className="mb-2">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Your Password"
-            className="p-3 border border-slate-600 bg-slate-700 focus:bg-slate-600 focus:text-slate-200 flex-grow rounded shadow"
-            onBlur={(e) => validateInputField(e, "password")}
-          />
-          {/* Error message */}
-          {!!password.error && (
-            <p className="text-red-500 mt-2 text-sm font-normal">
-              {password.error}
-            </p>
-          )}
-        </div>
-
-        {/* Confirm Password */}
-        <div className="mb-4 w-full flex flex-col">
-          <label htmlFor="confirmPassword" className="mb-2">
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            name="confirmPassword"
-            id="confirmPassword"
-            placeholder="Re-type your password"
-            className="p-3 border border-slate-600 bg-slate-700 focus:bg-slate-600 focus:text-slate-200 flex-grow rounded shadow"
-            onBlur={(e) => validateInputField(e, "confirmPassword")}
-          />
-          {/* Error message */}
-          {!!confirmPassErr && (
-            <p className="text-red-500 mt-2 text-sm font-normal">
-              {confirmPassErr}
-            </p>
-          )}
-        </div>
-
-        {/* submit button */}
-        <input
-          type="submit"
-          value="Sign Up"
-          className="w-full p-3 text-center shadow-md cursor-pointer rounded bg-sky-800 hover:bg-sky-700 text-sky-300 hover:text-sky-200 transition-all mt-4"
-        />
-      </form>
-
-      <p className="my-3 text-slate-400">
-        Already a user?{" "}
-        <Link to="/login" className="text-sky-400">
-          Sing In
-        </Link>
-      </p>
-
-      {/* one click log in  */}
-      <div className="px-4 flex flex-col items-center">
-        <span className="relative text-slate-500 mb-2 before:w-full after:w-full before:h-0.5 after:h-0.5 before:bg-slate-700 after:bg-slate-700 before:absolute after:absolute before:-left-full after:-right-full before:top-1/2 after:top-1/2 before:-ml-2 after:-mr-2">
-          Or Sign up with
-        </span>
-
-        <div className="flex gap-2 justify-center">
-          <button className="py-2 px-4 shadow bg-sky-800 hover:bg-sky-700 text-sky-400 hover:text-sky-300 transition-all rounded border-0">
-            Google
-          </button>
-          <button className="py-2 px-4 shadow bg-emerald-800 hover:bg-emerald-700 text-emerald-400 hover:text-emerald-300 transition-all rounded border-0">
-            Github
-          </button>
+          <div className="flex gap-2 justify-center">
+            <button className="py-2 px-4 shadow bg-sky-800 hover:bg-sky-700 text-sky-400 hover:text-sky-300 transition-all rounded border-0">
+              Google
+            </button>
+            <button className="py-2 px-4 shadow bg-emerald-800 hover:bg-emerald-700 text-emerald-400 hover:text-emerald-300 transition-all rounded border-0">
+              Github
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <Toaster />
+    </>
   );
 };
 
